73393306561263b6ae32e08b51f1aa54beb19c31
白云苍狗 <a888888hhb@outlook.com>
2023-08-09 22:03:54 +0800

    fix cors

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const response = NextResponse.next();
-
-	if (request.nextUrl.pathname.startsWith("/api")) {
-		response.headers.append("Access-Control-Allow-Origin", "*");
-		response.headers.append("Access-Control-Allow-Credentials", "true");
-		response.headers.append("Access-Control-Allow-Methods", "*");
-		response.headers.append("Access-Control-Allow-Headers", "*");
-	}
-
-	console.log(request.url);
-
-	return response;
+  console.log(request.url);
 }
 
 export const config = { matcher: ["/"] };
